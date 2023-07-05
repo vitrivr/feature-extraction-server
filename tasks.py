c3997d614c5a8e7090122e40163caaded9dd7ad0
faberf <fynn.f.faber@proton.me>
2023-07-05 09:00:38 +0200

    added automated speech recognition

--- tasks.py
+++ tasks.py
@@ -1,4 +1,4 @@
-from utils import prepare_images, prepare_text
+from utils import prepare_images, prepare_text, prepare_audio
 
 
 def caption(func):
@@ -19,9 +19,19 @@ def conditional_caption(func):
         return result
     return inner
 
+def automated_speech_recognition(func):
+    def inner(audio, inference_args={}):
+        audio, return_list = prepare_audio(audio)
+        result = func(audio, inference_args)
+        if not return_list:
+            result = result[0]
+        return result
+    return inner
+
 tasks = {
     "caption": caption,
-    "conditional_caption": conditional_caption
+    "conditional_caption": conditional_caption,
+    "automated_speech_recognition": automated_speech_recognition
 }
 
-default_models = {"caption": "blip", "conditional_caption": "blip"}
+default_models = {"caption": "blip2", "conditional_caption": "blip2", "automated_speech_recognition": "whisper"}
--- utils.py
+++ utils.py
@@ -2,6 +2,7 @@
 import base64
 import io
 from PIL import Image
+import soundfile as sf
 
 def prepare_images(images):
     return_list = True
@@ -28,8 +29,28 @@ def prepare_text(text):
         return_list = False
     return text, return_list
 
-
 def batch(iterable, n=1):
     l = len(iterable)
     for ndx in range(0, l, n):
         yield iterable[ndx:min(ndx + n, l)]
+
+def prepare_audio(audio_data_url):
+    return_list = True
+    audio_data = audio_data_url
+    if type(audio_data_url) is str:
+        audio_data = [audio_data_url]
+        return_list = False
+    audio = []
+    for audio_string in audio_data:
+        if audio_string.startswith('data:audio'):
+            audio_string = audio_string.split(',', 1)[1]
+        # decode the base64 data
+        binary_data = base64.b64decode(audio_string)
+
+        # create a binary stream from the data
+        data_stream = io.BytesIO(binary_data)
+
+        # read the audio file from the stream
+        samples, sample_rate = sf.read(data_stream)
+        audio.append((samples, sample_rate))
+    return audio, return_list
