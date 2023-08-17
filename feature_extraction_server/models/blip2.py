f062a593ed6713d4a0bf984f1813430abd8015cb
faberf <fynn.f.faber@proton.me>
2023-08-18 01:02:04 +0200

    fixed bug related to padding

--- feature_extraction_server/models/blip2.py
+++ feature_extraction_server/models/blip2.py
@@ -1,6 +1,7 @@
 import torch
 from transformers import Blip2Processor, Blip2ForConditionalGeneration, Blip2VisionModel, Blip2Model
 from feature_extraction_server.utils import batch
+import numpy as np
 
 defaults = {}
 
@@ -15,11 +16,14 @@ condgenmodel.to(device)
 
 
 def conditional_image_captioning(image, text, config={}):
+    
     # Set defaults if not provided
     args = defaults.copy()
     args.update(config)
     
-    inputs = processor(images=list(map(lambda x: x.to_numpy(), image)), text=text, return_tensors="pt")
+    image_tensor = list(map(lambda x: x.to_numpy(), image))
+    
+    inputs = processor(images=image_tensor, text=text, return_tensors="pt", padding=True)
     inputs = {k: v.to(condgenmodel.device) for k, v in inputs.items()}
     with torch.no_grad():
         output_ids = condgenmodel.generate(**inputs,  **args)
